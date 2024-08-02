76562464d3c14fd418878b3de1b0a0abbc99767e
Jake Dusing <dusingjake@gmail.com>
2024-08-02 18:40:16 -0500

    add form validation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,46 @@ export async function deleteSnippet(id: number) {
 
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // create a new record in the db
+    await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        mesage: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong..",
+      };
+    }
+  }
+
+  // redirect the user back to the root route
+  redirect("/");
+}
